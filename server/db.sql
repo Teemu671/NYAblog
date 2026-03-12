73bca19be4d0b39724d6caa9e33373fb929c0ec7
Teemu <willniko67@gmail.com>
2026-03-12 13:37:47 +0200

    adding sql tables

---
+++ server/db.sql
@@ -0,0 +1,33 @@
+--drop database if exists NYAdb;
+
+create database NYAdb;
+
+use NYAdb;
+
+create table "posts" (
+    "post_id" serial primary key,
+    "parent_id" int,
+    "author_id" int,
+    "text" varchar(255) not null,
+    "tag" varchar(16),
+    "created_at" timestamp not null default NOW(),
+    "updated_at" timestamp not null default NOW(),
+    CONSTRAINT fk_users FOREIGN KEY (user_id)
+    REFERENCES users(user_id)
+    ON DELETE CASCADE,
+    CONSTRAINT fk_post FOREIGN KEY (parent_id)
+    REFERENCES posts(post_id)
+    ON DELETE CASCADE
+);
+
+create table "users" (
+    "user_id" serial primary key,
+    "username" varchar(26) not null,
+    "password" varchar(255) not null,
+    "role" varchar(16) not null,
+    
+    "created_at" timestamp not null default NOW(),
+)
+
+
+insert into posts (parent_id,author_id) 
