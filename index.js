f7d9a8b339656626335956615eb4b11f3eafc575
Teemu671 <willniko67@gmail.com>
2026-04-29 02:29:36 +0300

    test

--- index.js
+++ index.js
@@ -8,23 +8,9 @@ const { DBpool } = require('./helpers/db.js')
 const APIPort = 3001;
 const httpsPort = 443;
 
-if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is running`);
 
-  // Fork workers.
-  cluster.fork();
-  cluster.fork();
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-  // setInterval(()=> {
-  //   console.log(DBpool.waitingCount)
-  // },1000)
-} else {
+api.listen(APIPort);
+httpsServer.listen(httpsPort);
 
-  api.listen(APIPort);
-  httpsServer.listen(httpsPort);
-
-  console.log(`Worker ${process.pid}  started`);
-}
+ 
