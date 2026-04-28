3d59fa1f8dfae3a495b59ee6de4e9670c638541d
Teemu671 <willniko67@gmail.com>
2026-04-29 02:45:35 +0300

    tes

--- routes/blogprotected.js
+++ routes/blogprotected.js
@@ -16,8 +16,8 @@ blogPRouter.post("/post",async(req,res) => {
                 return res.status(400).json({message: "Tag error"}) 
                 
             }
-            const sql = "insert into posts (image_id, parent_id, author_id, text, tag) values ($1,$2,$3,$4,$5) returning post_id"
-            const result = await query(sql,[req.body.image,null,req.user.id,req.body.message,req.body.tag])
+            const sql = "insert into posts (image_id, parent_id, author_id, text, tag, title) values ($1,$2,$3,$4,$5,$6) returning post_id"
+            const result = await query(sql,[req.body.image,null,req.user.id,req.body.message,req.body.tag,req.body.title])
             return res.status(200).json({message: "Successfully posted"}) 
         } else {
             return res.status(400).json({message: "Image error"}) 
@@ -50,8 +50,8 @@ blogPRouter.patch("/edit/:postID",async(req,res) => {
     try {
         const check = await query("select uploader_id from images where image_id = $1",[req.body.image])
         if (check.rowCount === 1 || req.body.image == null) {
-            const sql = "update posts set image_id = $1, text = $2, tag = $3 where author_id = $4 and post_id = $5 returning post_id"
-            const result = await query(sql,[req.body.image,req.body.message,req.body.tag,req.user.id,req.params.postID])
+            const sql = "update posts set image_id = $1, text = $2, tag = $3, title = $4 where author_id = $5 and post_id = $6 returning post_id"
+            const result = await query(sql,[req.body.image,req.body.message,req.body.tag,req.body.title,req.user.id,req.params.postID])
             if (result.rows[0]){
                 return res.status(200).json({message: "Successfully edited"}) 
             }
