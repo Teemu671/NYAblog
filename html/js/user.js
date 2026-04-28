f4763f3c7f4d21d71c076f8e83ea523f3876a6a4
Teemu671 <willniko67@gmail.com>
2026-04-28 21:49:42 +0300

    e

--- html/js/user.js
+++ html/js/user.js
@@ -1,13 +1,3 @@
 import { User } from './class/user.js'
 
-const login_link_a = document.querySelector('a#login-link')
-
 const user = new User()
-
-if (user.isLoggedIn) {
-  login_link_a.innerHTML = "Logout"
-  login_link_a.href="logout.html"
-} else {
-  login_link_a.innerHTML = "Login"
-  login_link_a.href="login.html"
-}
