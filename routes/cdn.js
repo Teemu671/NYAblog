07ee52a60de9c4c96212d28c2a90c70a2bad4eac
Teemu671 <willniko67@gmail.com>
2026-04-29 00:40:13 +0300

    e

--- routes/cdn.js
+++ routes/cdn.js
@@ -8,7 +8,7 @@ cdnRouter.get("/image/:imageID", async (req, res) => {
         const sql = "SELECT filename FROM images WHERE image_id = $1"
         const result = await query(sql, [req.params.imageID])
         if (result.rowCount === 0) return res.status(404).json({ error: "Not found" })
-        res.sendFile(result.rows[0].filename)
+        res.sendFile(result.rows[0])
     } catch (error) {
         res.status(500).json({ error: "Server error" })
     }
--- routes/cdnprotected.js
+++ routes/cdnprotected.js
@@ -50,15 +50,5 @@ cdnPRouter.get("/gallery",async(req,res) => {
     
 })
 
-cdnPRouter.get("/image/:imageID", async (req, res) => {
-    try {
-        const sql = "SELECT filename FROM images WHERE image_id = $1"
-        const result = await query(sql, [req.params.imageID])
-        if (result.rowCount === 0) return res.status(404).json({ error: "Not found" })
-        res.sendFile(result.rows[0].filename)
-    } catch (error) {
-        res.status(500).json({ error: "Server error" })
-    }
-})
 
 module.exports = { cdnPRouter }
