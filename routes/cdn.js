b4545b2130264019caa9f005c3b5e0addc63cf48
Teemu671 <willniko67@gmail.com>
2026-04-29 01:07:46 +0300

    e

--- routes/cdn.js
+++ routes/cdn.js
@@ -8,7 +8,7 @@ cdnRouter.get("/image/:imageID", async (req, res) => {
         const sql = "SELECT filename FROM images WHERE image_id = $1"
         const result = await query(sql, [req.params.imageID])
         if (result.rowCount === 0) return res.status(404).json({ error: "Not found" })
-        const rows = result.rows ? result.rows : []
+        const rows = result.rows ? result.rows[0] : []
         res.status(200).json(rows)
     } catch (error) {
         res.status(500).json({ error: error })
