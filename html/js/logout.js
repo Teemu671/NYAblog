7069351434106d82f7785f6f345b6802ba4003d9
Teemu671 <willniko67@gmail.com>
2026-04-29 06:09:50 +0300

    e

--- html/js/logout.js
+++ html/js/logout.js
@@ -3,11 +3,10 @@ const logout = () => {
         document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); 
     });
 
-
     // Clear localStorage and sessionStorage
     localStorage.clear();
     sessionStorage.clear();
 
     // Redirect to login page
-    router.push("/login",{"message":"Invalid login."})
+    router.push("/login")
 };
